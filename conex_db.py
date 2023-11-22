afa581a74096c28a6b24644105c5a59a19c1cd4c
TenorioMartínezLeonardo <119890042+DowncastList1@users.noreply.github.com>
2023-11-22 09:21:10 -0600

    V2

--- conex_db.py
+++ conex_db.py
@@ -1,69 +1,32 @@
 import mysql.connector
 
-
-db = None
-cursor = None
-
-db_name = 'db_poo'
-
-
 class database:
 
-    db = None
-    cursor = None
-
     def __init__(self):
-        global cursor
+        self.db = None
+        self.cursor = None
 
         try:
-            db = mysql.connector.connect(
+            self.db = mysql.connector.connect(
                 host='localhost',
                 user='root',
                 password='',
                 db='db_poo'
             )
-            cursor = db.cursor()
+            
+            self.cursor = self.db.cursor()
 
-            if db.is_connected():
+            if self.db.is_connected():
                 print("Conexion Exitosa....")
 
         except Exception as ex:
             print(ex)
             print("Error en la conexión a la base de datos")
-
-    def createSchema():
-        try:
-            sql = "CREATE SCHEMA IF NOT EXISTS `vortex_ventures` DEFAULT CHARACTER SET utf8mb4 ;"
-            cursor.execute(sql)
-            sql = "USE vortex_ventures;"
-            cursor.execute(sql)
-        except Exception as ex:
-            print(ex)
-            print("Error al crear el esquema")
-
-    def createTableAuto():
-        sql = """CREATE TABLE IF NOT EXISTS `db_poo`.`autorizacion` (
-                    id` INT(5) NOT NULL,
-                    `revi_por` VARCHAR(100) NULL DEFAULT NULL,
-                    `autori-por` VARCHAR(100) NULL DEFAULT NULL,
-                    `requisicion_folio` VARCHAR(10) NOT NULL,
-                    PRIMARY KEY (`id`),
-                    INDEX `fk_autorizacion_requisicion1_idx` (`requisicion_folio`),
-                        CONSTRAINT `fk_autorizacion_requisicion1`
-                        FOREIGN KEY (`requisicion_folio`)
-                        REFERENCES `db_poo`.`requisicion` (`folio`)
-                        ON DELETE NO ACTION
-                        ON UPDATE NO ACTION)
-                ENGINE = InnoDB
-                DEFAULT CHARACTER SET = utf8mb4;"""
-        cursor.execute(sql)
-        print("Tabla autorizacion creada con exito")
-
-    def close_conex():
-        if cursor:
-            cursor.close()
-        if db:
-            db.close()
-
+        
+    def close_conex(self):
+        if self.cursor:
+            self.cursor.close()
+        if self.db:
+            self.db.close()
 
 db = database()
