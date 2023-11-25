61108d5836a5f8372e85b6a7f07ad59fe9bc820e
TenorioMartínezLeonardo <119890042+DowncastList1@users.noreply.github.com>
2023-11-25 00:15:13 -0600

    drag

--- drag.py
+++ drag.py
@@ -1,1 +1,8 @@
-drag.py
+def mousePressEvent(self, event):
+    self.dragPos = event.globalPosition().toPoint()
+
+
+def mouseMoveEvent(self, event):
+    self.move(self.pos() + event.globalPosition().toPoint() - self.dragPos)
+    self.dragPos = event.globalPosition().toPoint()
+    event.accept()
