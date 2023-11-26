e822d0655a9fad8fdb7a42f8f5a1ee348ebe0a19
MayrinReyes <129233059+MayrinReyes@users.noreply.github.com>
2023-11-25 23:16:00 -0600

    Update barra_carga.py

--- barra_carga.py
+++ barra_carga.py
@@ -11,11 +11,12 @@ class Window(QWidget):
 
         # Calculate the position of the window in the center
         screen_geometry = QApplication.primaryScreen().availableGeometry()
-        window_width = 600
-        window_height = 800
+        window_width = 350
+        window_height = 150
         window_x = (screen_geometry.width() - window_width) // 2
         window_y = (screen_geometry.height() - window_height) // 2
         self.setGeometry(window_x, window_y, window_width, window_height)
+        self.setStyleSheet("background-color: #161616")
 
         self.prog_bar = QProgressBar(self)
         self.prog_bar.setGeometry(50, 50, 250, 30)
@@ -24,14 +25,22 @@ class Window(QWidget):
         self.timer.timeout.connect(self.auto_increment)
         self.timer.start(500)  # auto-increment every 1000 milliseconds
 
-        self.close_button = QPushButton("Close", self)
+        self.close_button = QPushButton("X", self)
         self.close_button.clicked.connect(self.close)
         self.close_button.setGeometry(250, 10, 60, 30)
 
-        self.minimize_button = QPushButton("Minimize", self)
+        self.minimize_button = QPushButton("-", self)
         self.minimize_button.setObjectName("minimize_button")
         self.minimize_button.clicked.connect(self.showMinimized)
         self.minimize_button.setGeometry(180, 10, 60, 30)
+
+    def mousePressEvent(self, event):
+        self.dragPos = event.globalPosition().toPoint()
+
+    def mouseMoveEvent(self, event):
+        self.move(self.pos() + event.globalPosition().toPoint() - self.dragPos)
+        self.dragPos = event.globalPosition().toPoint()
+        event.accept()
 
     def auto_increment(self):
         value = self.prog_bar.value()
@@ -41,9 +50,8 @@ class Window(QWidget):
 if __name__ == '__main__':
     with open('styles.css', 'r') as f:
         style = f.read()
-
-    app = QApplication(sys.argv)
-    app.setStyleSheet(style)  # Aplicar el estilo global
+    app = QApplication(sys.argv)##administrar todo lo que se haga en la venta
+    app.setStyleSheet(style)
     ventana = Window()
     ventana.show()
     sys.exit(app.exec())
