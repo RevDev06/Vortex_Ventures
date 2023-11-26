cff6759e306a1b32feed833b2d83eb3e1f7b9390
RavenShiro <148567781+RavenShiro@users.noreply.github.com>
2023-11-25 18:15:07 -0600

    login.py

--- login.py
+++ login.py
@@ -1,19 +1,22 @@
 import sys
 from PyQt6.QtWidgets import *
 from PyQt6.QtCore import *
+from PyQt6.QtGui import  QBrush, QLinearGradient, QPainter, QColor
 from PyQt6.QtGui import *
 from werkzeug.security import generate_password_hash, check_password_hash
 from conex_db import database
 from home import HomeWindow
 
 con_db = database()
-con_db.verifAndCreateDataBase()
+
 
 class LoginScreen(QMainWindow):
     def __init__(self):
         super().__init__()
 
+
         self.setWindowFlags(Qt.WindowType.FramelessWindowHint)
+
 
         screen_geometry = QApplication.primaryScreen().availableGeometry()
         window_width = 1000
@@ -21,7 +24,6 @@ class LoginScreen(QMainWindow):
         window_x = (screen_geometry.width() - window_width) // 2
         window_y = (screen_geometry.height() - window_height) // 2
         self.setGeometry(window_x, window_y, window_width, window_height)
-        self.setObjectName("fondo")
 
         central_widget = QWidget(self)
         self.setCentralWidget(central_widget)
@@ -36,22 +38,24 @@ class LoginScreen(QMainWindow):
         palette.setBrush(QPalette.ColorGroup.All, QPalette.ColorRole.Window, QBrush(gradient))
         self.setPalette(palette)
 
+
         self.icon = QIcon
 
         self.tittle = QLabel("LOGIN", self)
-        self.tittle.setGeometry(480, window_height // 2 - 300, 100, 30)
+        self.tittle.setGeometry(475, window_height // 2 - 250, 100, 30)
         self.tittle.setFont(QFont('vintage',16))
 
         self.username_label = QLabel("Username:", self)
         self.username_label.setGeometry(340, window_height // 2 - 200, 100, 30)
         self.username_label.setFont(QFont('Arial',12))
 
+
         self.username_input = QLineEdit(self)
         self.username_input.setGeometry(420, window_height // 2 - 200, 200, 30)
 
         self.username_feedback = QLabel("", self)
-        self.username_feedback.setGeometry(
-            430, window_height // 2 - 170, 300, 30)
+        self.username_feedback.setGeometry(430, window_height // 2 - 170, 300, 30)
+
 
         self.password_label = QLabel("Password:", self)
         self.password_label.setGeometry(340, window_height // 2 - 100, 100, 30)
@@ -62,11 +66,10 @@ class LoginScreen(QMainWindow):
         self.password_input.setEchoMode(QLineEdit.EchoMode.Password)
 
         self.password_feedback = QLabel("", self)
-        self.password_feedback.setGeometry(
-            430, window_height // 2 - 70, 300, 30)
+        self.password_feedback.setGeometry(430, window_height // 2 - 70, 300, 30)
 
         self.login_button = QPushButton("Login", self)
-        self.login_button.setGeometry(440, window_height // 2 - 20, 120, 40)
+        self.login_button.setGeometry(430, window_height // 2- 20 , 140, 60)
         self.login_button.clicked.connect(self.login)
         self.login_button.setFont(QFont('Arial',10))
 
@@ -80,27 +83,18 @@ class LoginScreen(QMainWindow):
 
         self.register_button = QPushButton("Register Now", self)
         self.register_button.clicked.connect(self.red_register)
-        self.register_button.setGeometry(440, window_height // 2 + 30, 120, 40)
+        self.register_button.setGeometry(430, window_height // 2 + 60, 140, 60)
         self.register_button.setFont(QFont('Arial',10))
 
         self.tittle.setStyleSheet("color:#251d1c;")
-        self.username_label.setStyleSheet(" color:#816d50;")
-        self.password_label.setStyleSheet(";color:#816d50;")
+        self.username_label.setStyleSheet("font-family:Harrison; color:#816d50;")
+        self.password_label.setStyleSheet(" font-family: vintage;;color:#816d50;")
 
         self.login_button.setStyleSheet("background-color: #43423b;color:#d2c499;border-radius: 5px;")
         self.close_button.setStyleSheet("background-color: #fae9cc; color: white;")
         self.minimize_button.setStyleSheet("background-color: #d2c499; color: black;")
         self.register_button.setStyleSheet("background-color: #816d50;color:#122324; border-radius: 5px;")
 
-    def mousePressEvent(self, event):
-        self.dragPos = event.globalPosition().toPoint()
-
-    def mouseMoveEvent(self, event):
-        self.move(self.pos() + event.globalPosition().toPoint() - self.dragPos)
-        self.dragPos = event.globalPosition().toPoint()
-        event.accept()
-
-        
     def login(self):
         username = self.username_input.text()
         password = self.password_input.text()
@@ -113,8 +107,7 @@ class LoginScreen(QMainWindow):
 
         if result is None:
             print("Nombre de usuario incorrecto. Vuelve a intentarlo")
-            self.username_feedback.setText(
-                "Nombre de usuario incorrecto. Vuelve a intentarlo")
+            self.username_feedback.setText("Nombre de usuario incorrecto. Vuelve a intentarlo")
         else:
             if password == db_password:
                 print("Bienvenido")
@@ -126,10 +119,11 @@ class LoginScreen(QMainWindow):
                 self.password_feedback.setText("Contraseña incorrecta.")
                 print("Acesso denegado")
         return False
+    
 
     def red_register(self):
         print("Redireccioando")
-
+    
     def paintEvent(self, event):
        # Pintar un cuadrado en la ventana
         painter = QPainter(self)
@@ -143,7 +137,7 @@ class LoginScreen(QMainWindow):
         painter.fillRect(290, 130, 430, 430, color_fondo)
 
         # Ajustar el brillo del color original para el borde
-        brillo_borde = 500
+        brillo_borde = 1000
         color_borde = color_original.lighter(brillo_borde)
 
         # Dibujar un rectángulo más grande para el borde
@@ -151,12 +145,18 @@ class LoginScreen(QMainWindow):
         painter.setPen(color_borde)  # Establecer el color del borde
         painter.drawRect(270, 110, 470, 470)
 
+
 def main():
+    with open('styles.css', 'r') as f:
+        style = f.read()
     app = QApplication(sys.argv)
+    app.setStyleSheet(style) 
     login_screen = LoginScreen()
+    
     login_screen.show()
     sys.exit(app.exec())
 
 
-if __name__ == '__main__':
+if __name__== '__main__':
     main()
+        
